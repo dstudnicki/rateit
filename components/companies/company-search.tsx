7abc4f3d7e0cabe4afdcf80d8fb0e6d8a5aba2d2
dstudnicki <domistu@poczta.fm>
2026-01-21 00:18:03 +0100

    fix pl localization

--- components/companies/company-search.tsx
+++ components/companies/company-search.tsx
@@ -36,7 +36,7 @@ export function CompanySearch() {
                     <div className="relative">
                         <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                         <Input
-                            placeholder="Nazwa firmy lub ID"
+                            placeholder="Nazwa firmy"
                             value={companyName}
                             onChange={(e) => setCompanyName(e.target.value)}
                             className="pl-9"
